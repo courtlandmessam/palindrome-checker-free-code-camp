dbd8c660f20be2bd79ff74cf91b29f36bb143038
courtlandmessam <messamcourtland@gmail.com>
2020-01-03 08:28:31 -0500

    Added palindrome checker logic

--- palindrome.js
+++ palindrome.js
@@ -1,7 +1,23 @@
 function palindrome(str) {
-  return true;
+  //Removes nonalphanumeric characters and makes characters upper case----------
+  str = str.match(/\w*/g);
+  str = str.join("");
+  str = str.replace("_", "");
+  str = str.toUpperCase();
+
+  //Creates a reverse string to compare-----------------------------------------
+  let reverseStr = str.split("");
+  reverseStr = reverseStr.reverse();
+  reverseStr = reverseStr.join("");
+
+  //Compares strings------------------------------------------------------------
+  if (str === reverseStr) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
 
-palindrome("eye");
+palindrome("2_erferf@#$@#$!#$! A3*3#A2");
